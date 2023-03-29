28a486857dc14aa058bbd4de35339d83ab4a962e
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-03-29 15:10:38 +0200

    Toolkit test

--- IWA_02_Comments_and_Logging/js/IWA_2_challenges.js
+++ IWA_02_Comments_and_Logging/js/IWA_2_challenges.js
@@ -27,3 +27,6 @@ console.warn("Security scan starting");
 // It is important to let user know when they can close the page  /*
 
 console.info("Please wait for scan to complete before closing the browser.");
+/**
+ (c) ACME Inc. 2010 */
+let name = "Tirelo";
