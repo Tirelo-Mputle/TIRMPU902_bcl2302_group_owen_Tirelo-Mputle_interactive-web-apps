06e7937f84d07384c928002da39eb93329dbeff8
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-03-30 11:38:43 +0200

    Completed calcTotalCost function

--- IWA_05_Branching_Logic/js/scripts.js
+++ IWA_05_Branching_Logic/js/scripts.js
@@ -1,81 +1,80 @@
+//WARNINGS
 const FREE_WARNING =
-  "Free shipping only applies to single customer orders from South Africa or Namibia.";
+  "Free shipping only applies to single customer orders from South Africa (minimum R1000) or Namibia (minimum $60).";
 const BANNED_WARNING =
   "Unfortunately we do not ship to your country of residence";
-const NONE_SELECTED = 0;
+const FREE_SHIPPING = "Your order qualifies for free shipping!";
 
-// let location;
-const OTHER_COUNTRIES = "Other countries";
-const RSA = "South Africa";
-const NAM = "Namibia";
-const NK = "North Korea";
-let currency = null;
-let customers = 1;
-
-const calcShipping = (location, currency) => {
-  let shipping;
-  if (location === OTHER_COUNTRIES && currency === "$") {
-    shipping = 800;
-    console.log(shipping);
-    return shipping;
-  }
-
-  if (location === RSA && currency === "R") {
-    shipping = 400;
-    console.log(shipping);
-    return shipping;
-  }
-
-  if (location === NAM && currency === "$") {
-    shipping = 600;
-    console.log(location, shipping);
-    return shipping;
-  }
-  if (location === NK) {
-    console.log(BANNED_WARNING);
-  }
-};
-
+//SHIPPING DETAILS
 const MIN_SHIPPING_DOLLARS = 60;
 const MIN_SHIPPING_RANDS = 1000;
+const OTHER_COUNTRIES_SHIPPING = 800;
+const RSA_SHIPPING = 400;
+const NAM_SHIPPING = 600;
 
-const calcTotalCosts = (location, currency) => {
-  const shippingCost = calcShipping(location, currency);
-  const free_shipping_dollars =
-    currency === "$" && shippingCost >= MIN_SHIPPING_DOLLARS;
-  console.log(free_shipping_dollars);
-  const free_shipping_rands =
-    currency === "R" && shippingCost >= MIN_SHIPPING_RANDS;
-
-  calcShipping(location, currency);
-  if (location === RSA || location === NAM) {
-    if ((free_shipping_dollars || free_shipping_rands) && customers === 1) {
-      console.log("shipping free");
-      console.log(location, currency, shippingCost);
-    } else {
-      console.log(FREE_WARNING);
-    }
-  }
-};
-
-calcTotalCosts(NAM, "$");
-
+// SHOPPING ITEMS DETAILS
+const NONE_SELECTED = 0;
 const shoes = 300 * 1;
 const toys = 100 * 5;
 const shirts = 150 * NONE_SELECTED;
 const batteries = 35 * 2;
 const pens = 5 * NONE_SELECTED;
+//COST OF ITEMS
+const TOTAL_COST_ITEMS = shoes + batteries + pens + shirts + toys;
+const RAND_TO_DOLLAR_EXCHANGE = 18;
+const ITEM_COSTS_RANDS = TOTAL_COST_ITEMS;
+const ITEM_COSTS_DOLLARS = Math.floor(
+  TOTAL_COST_ITEMS / RAND_TO_DOLLAR_EXCHANGE
+);
+console.log(ITEM_COSTS_DOLLARS);
 
-const totalCostOfItems = shoes + batteries + pens + shirts + toys + 400;
-console.log(totalCostOfItems);
-// if (shoes + batteries + pens + shirts > 1000 &&  ) {
-// 	if (location === NAM && customers < 2) {
-// 			if (location === RSA)
-// 		    shipping = 0 || calcShipping
-// 		}
-// 	}
-// }
+// lOCATIONS AND CUSTOMER DETAILS;
+const OTHER_COUNTRIES = "Other countries";
+const RSA = "South Africa";
+const NAM = "Namibia";
+const NK = "North Korea";
 
-// if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+let customers = 1;
 
-// location === 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+const calcTotalCosts = (location, currency) => {
+  //Check if item costs are more than the minimum free shipping price
+  const free_shipping_dollars =
+    currency === "$" && ITEM_COSTS_DOLLARS >= MIN_SHIPPING_DOLLARS;
+
+  const free_shipping_rands =
+    currency === "R" && ITEM_COSTS_RANDS >= MIN_SHIPPING_RANDS;
+
+  if (location == NK) {
+    console.log(BANNED_WARNING);
+    return;
+  }
+  if (location !== RSA && location !== NAM) {
+    console.log(FREE_WARNING);
+    return;
+  }
+
+  if (location === RSA && free_shipping_rands && customers === 1) {
+    console.log(FREE_SHIPPING);
+    console.log(
+      `Location: ${location}. Price: ${currency} ${ITEM_COSTS_RANDS}`
+    );
+  } else if (location === NAM && free_shipping_dollars && customers === 1) {
+    console.log(FREE_SHIPPING);
+    console.log(
+      `Location: ${location}. Price: ${currency} ${ITEM_COSTS_DOLLARS}`
+    );
+  } else {
+    const checkoutMessage =
+      location === RSA
+        ? `Location: ${location}. Your order amount is:${currency}${ITEM_COSTS_RANDS}. Plus ${currency}${RSA_SHIPPING} shipping is: ${currency}${
+            ITEM_COSTS_RANDS + RSA_SHIPPING
+          }`
+        : `Location: ${location}. Your order is:${currency}${ITEM_COSTS_DOLLARS}. Plus ${currency}${NAM_SHIPPING} shipping is: ${currency}${
+            ITEM_COSTS_DOLLARS + NAM_SHIPPING
+          }`;
+    console.log(FREE_WARNING);
+    console.log(checkoutMessage);
+  }
+};
+
+calcTotalCosts(RSA, "R");
