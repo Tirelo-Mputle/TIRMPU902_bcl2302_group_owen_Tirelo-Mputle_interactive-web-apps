2e27325403a1f2007573e7178ff9cc2f21ce24cf
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-03-31 14:40:56 +0200

    Completed challenge 1

---
+++ IWA_07_Strings_and_Logical_Operators/IWA_07_challenge_1/js/scripts.js
@@ -0,0 +1,7 @@
+const value = 3;
+console.log(value + 4 + value);
+
+/* Value is the string "3". When concatenating a sting with
+    a number, the number will be coerced into a string. 
+    This results in three strings being put together to form a
+    three character string. */
