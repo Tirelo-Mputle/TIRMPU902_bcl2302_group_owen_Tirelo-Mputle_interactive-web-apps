86639b10bca6c7bff9dd11528ea05c6ce03f4444
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-04-05 09:43:07 +0200

    Refactored solution and added explanatory comments

--- IWA_06_Type_Coercion/IWA_06_challenge_1/js/scripts.js
+++ IWA_06_Type_Coercion/IWA_06_challenge_1/js/scripts.js
@@ -3,11 +3,24 @@ const secondaryPhone = "0219131568";
 
 // Only change below this line
 
-const primaryValid =
-  typeof parseInt(primaryPhone) === "number" && !isNaN(parseInt(primaryPhone));
-const secondaryValid =
-  typeof parseInt(secondaryPhone) === "number" &&
-  !isNaN(parseInt(secondaryPhone));
+// const primaryValid =
+//   typeof parseInt(primaryPhone) === "number" && !isNaN(parseInt(primaryPhone));
+// const secondaryValid =
+//   typeof parseInt(secondaryPhone) === "number" &&
+//   !isNaN(parseInt(secondaryPhone));
+
+// Refactored code
+const primaryValid = !isNaN(parseInt(primaryPhone));
+const secondaryValid = !isNaN(parseInt(secondaryPhone));
 
 console.log("Primary phone is valid numerical string:", primaryValid);
 console.log("Secondary phone is valid numerical string:", secondaryValid);
+/* 
+- ParseInt return a number or NaN.
+- Check if that value is NOT NaN
+-primaryPhone is NaN. The check if for is
+it is NOT Nan so it will return false.
+-secondayPhone is a number so the check will
+return true because it it NOT NaN
+
+*/
