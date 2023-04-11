3f54eae96d15d5b2ccf093b047a00fd63e2c564a
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-04-11 11:29:21 +0200

    Completed IWA9 challenge 1

---
+++ IWA_09_Using_Objects/js/scripts.js
@@ -0,0 +1,40 @@
+console.log("hello");
+
+const salary = 4000;
+const lodging = "apartment";
+const size = "large";
+
+// Only change the syntax below (not the values or key names)
+
+const expenses = {
+  food: 51.7501,
+  transport: 10.2,
+};
+
+const tax = {
+  734: "3%",
+  234: "20%",
+  913: "12%",
+  415: "38%",
+  502: "42%",
+};
+
+const rent = {
+  none: 0,
+  "small-room": 200,
+  "large-room": 300,
+  "small-apartment": 400,
+  "large-apartment": 800,
+  "small-house": 1200,
+  "large-house": 2400,
+};
+
+// You can change below however you want
+
+const taxAsDecimal =
+  parseFloat(tax["913"].substring(0, tax["913"].length - 1)) / 100;
+const startingAfterTax = salary - taxAsDecimal * salary;
+const type = `${size}-${lodging}`;
+const sumOfExpenses = expenses.food + expenses.transport + rent[type];
+const balance = (startingAfterTax - sumOfExpenses).toFixed(2);
+console.log(balance);
