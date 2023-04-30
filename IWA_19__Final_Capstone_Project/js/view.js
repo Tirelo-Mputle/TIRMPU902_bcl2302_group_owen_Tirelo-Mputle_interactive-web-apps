2bab8140fd73982c41192d558f85430d1c646225
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-04-30 14:43:50 +0200

    Added view.js file
    Selected and exported html elements

---
+++ IWA_19__Final_Capstone_Project/js/view.js
@@ -0,0 +1,38 @@
+const html = {
+  header: {
+    search: document.querySelector("[data-header-search]"),
+    settings: document.querySelector("[data-header-settings]"),
+  },
+  list: {
+    items: document.querySelector("[data-list-items]"),
+    message: document.querySelector("[data-list-message]"),
+    button: document.querySelector("[data-list-button]"),
+  },
+  active: {
+    overlay: document.querySelector("[data-list-active]"),
+    blur: document.querySelector("[data-list-blur]"),
+    image: document.querySelector("[data-list-image]"),
+    title: document.querySelector("[data-list-title]"),
+    subtitle: document.querySelector("[data-list-subtitle]"),
+    description: document.querySelector("[data-list-description]"),
+    close: document.querySelector("[data-list-close]"),
+  },
+  search: {
+    overlay: document.querySelector("[data-search-overlay]"),
+    form: document.querySelector("[data-search-form]"),
+    title: document.querySelector("[data-search-title]"),
+    genres: document.querySelector("[data-search-genres]"),
+    authors: document.querySelector("[data-search-authors]"),
+    cancel: document.querySelector("[data-search-cancel]"),
+    //todo add attribute to index.html
+    // submit: document.querySelector("[data-search-submit]"),
+  },
+  settings: {
+    overlay: document.querySelector("[data-settings-overlay]"),
+    form: document.querySelector("[data-settings-form]"),
+    theme: document.querySelector("[data-settings-theme]"),
+    cancel: document.querySelector("[data-settings-cancel]"),
+    //todo add attribute to index.html
+    // save: document.querySelector("[data-settings-save]"),
+  },
+};
